b04ec25551db2ba3c804fd8902bf1e5c3860847a
Elliott Slaughter <slaughter@cs.stanford.edu>
2024-03-24 15:06:02 -0700

    Basic test harness infrastructure.

---
+++ runner.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+
+# Copyright 2024 Stanford University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import argparse, multiprocessing, os, queue, shlex, subprocess
+
+def run_fuzzer(seed, num_ops, fuzzer_exe):
+    cmd = [fuzzer_exe, '-fuzz:seed', str(seed), '-fuzz:ops', str(num_ops), '-level', '4']
+    proc = subprocess.run(cmd, capture_output=True)
+    if proc.returncode == 0:
+        return
+    return proc
+
+def bisect(thread_index, thread_count, seed, num_ops, fuzzer_exe, verbose):
+    if verbose:
+        print(f'[{thread_index} of {thread_count}]: Bisecting {num_ops} ops at seed {seed} to find shortest failure')
+    good = 0
+    bad = num_ops
+    while good + 1 < bad:
+        check = (good + bad)//2
+        if verbose:
+            print(f'[{thread_index} of {thread_count}]: Testing {check} ops at seed {seed}')
+        proc = run_fuzzer(seed, check, fuzzer_exe)
+        if proc is None:
+            good = check
+        else:
+            bad = check
+    return bad, proc
+
+def fuzz(thread_index, thread_count, seed, num_ops, fuzzer_exe, verbose):
+    if verbose:
+        print(f'[{thread_index} of {thread_count}]: Testing {num_ops} ops at seed {seed}')
+    proc = run_fuzzer(seed, num_ops, fuzzer_exe)
+    if proc is None:
+        return
+    print(f'[{thread_index} of {thread_count}]: Found failure: {shlex.join(proc.args)}')
+    bad, bad_proc = bisect(thread_index, thread_count, seed, num_ops, fuzzer_exe, verbose)
+    if bad_proc:
+        print(f'[{thread_index} of {thread_count}]: Shortest failure for this seed: {shlex.join(bad_proc.args)}')
+    else:
+        print(f'[{thread_index} of {thread_count}]: No way to shorten failure for this seed')
+    return bad_proc or proc
+
+def run_test(thread_index, thread_count, num_tests, num_ops, base_seed, fuzzer_exe, spy_script, verbose):
+    print(f'[{thread_index} of {thread_count}]: Starting search')
+    initial_seed = base_seed + thread_index
+    failed_procs = []
+    for test_index in range(num_tests):
+        seed = initial_seed + test_index * thread_count
+        proc = fuzz(thread_index, thread_count, seed, num_ops, fuzzer_exe, verbose)
+        if proc:
+            failed_procs.append(proc)
+    return failed_procs
+
+def report_failure(proc):
+    print(f'Captured failure: {shlex.join(proc.args)}')
+    print(proc.stderr.decode('utf-8'))
+    print()
+
+def run_tests(thread_count, num_tests, num_ops, base_seed, fuzzer_exe, spy_script, verbose):
+    if thread_count is None:
+        thread_count = os.cpu_count()
+
+    thread_pool = multiprocessing.Pool(thread_count)
+
+    result_queue = queue.Queue()
+    num_queued = 0
+    for thread_index in range(thread_count):
+        num_queued += 1
+        def callback(r):
+            result_queue.put(r)
+        def error_callback(e):
+            print('ERROR CALLBACK', e)
+            raise e
+        thread_pool.apply_async(
+            run_test,
+            (thread_index, thread_count, num_tests, num_ops, base_seed, fuzzer_exe, spy_script, verbose),
+            callback=callback,
+            error_callback=error_callback)
+
+    thread_pool.close()
+
+    num_remaining = num_queued
+    try:
+        while num_remaining > 0:
+            failed_procs = result_queue.get()
+            for proc in failed_procs:
+                report_failure(proc)
+            num_remaining -= 1
+        thread_pool.join()
+    except KeyboardInterrupt:
+        thread_pool.terminate()
+        raise
+
+def driver():
+    parser = argparse.ArgumentParser(description='Fuzzer test harness')
+    parser.add_argument('-j',
+                        nargs='?',
+                        type=int,
+                        help='number threads used to test',
+                        dest='thread_count')
+    parser.add_argument('-n', '--tests',
+                        type=int,
+                        default=10,
+                        dest='num_tests',
+                        help='number of tests to run per seed')
+    parser.add_argument('-o', '--ops',
+                        type=int,
+                        default=100,
+                        dest='num_ops',
+                        help='number of operations to run per test')
+    parser.add_argument('-s', '--seed',
+                        type=int,
+                        default=0,
+                        dest='base_seed',
+                        help='base seed to use')
+    parser.add_argument('--fuzzer',
+                        required=True,
+                        dest='fuzzer_exe',
+                        help='location of fuzzer executable')
+    parser.add_argument('--spy',
+                        dest='spy_script',
+                        help='location of Legion Spy script (optional)')
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        dest='verbose',
+                        help='enable verbose output')
+    args = parser.parse_args()
+    run_tests(**vars(args))
+
+if __name__ == '__main__':
+    driver()
--- test.sh
+++ test.sh
@@ -3,41 +3,48 @@
 set -e
 set -x
 
-if [[ $INSTALL_DEPS -eq 1 ]]; then
+FUZZER_INSTALL_DEPS=${FUZZER_INSTALL_DEPS:-0}
+FUZZER_INSTALL_LEGION=${FUZZER_INSTALL_LEGION:-1}
+FUZZER_DEBUG=${FUZZER_DEBUG:-1}
+FUZZER_THREADS=${FUZZER_THREADS:-4}
+
+if [[ $FUZZER_INSTALL_DEPS -eq 1 ]]; then
     $SUDO_COMMAND apt-get update -qq
     $SUDO_COMMAND apt-get install -qq mpich libmpich-dev
 fi
 
-if [[ ! -e legion ]]; then
-    git clone https://gitlab.com/StanfordLegion/legion.git
-fi
+if [[ $FUZZER_INSTALL_LEGION -eq 1 ]]; then
+    if [[ ! -e legion ]]; then
+        git clone https://gitlab.com/StanfordLegion/legion.git
+    fi
 
-pushd legion
-if [[ ! -e build ]]; then
-    mkdir build
-    pushd build
-    legion_flags=(
-        -DCMAKE_BUILD_TYPE=$([ ${FUZZER_DEBUG:-1} -eq 1 ] && echo Debug || echo Release)
-        -DCMAKE_INSTALL_PREFIX=$PWD/../install
-        -DCMAKE_CXX_STANDARD=17
-        -DLegion_SPY=ON
-        -DBUILD_SHARED_LIBS=ON # to improve link speed
-    )
-    if [[ ${FUZZER_DEBUG:-1} -eq 1 ]]; then
-        legion_flags+=(
-            -DBUILD_MARCH= # to avoid -march=native for valgrind compatibility
+    pushd legion
+    if [[ ! -e build ]]; then
+        mkdir build
+        pushd build
+        legion_flags=(
+            -DCMAKE_BUILD_TYPE=$([ $FUZZER_DEBUG -eq 1 ] && echo Debug || echo Release)
+            -DCMAKE_INSTALL_PREFIX=$PWD/../install
+            -DCMAKE_CXX_STANDARD=17
+            -DLegion_SPY=ON
+            -DBUILD_SHARED_LIBS=ON # to improve link speed
         )
+        if [[ $FUZZER_DEBUG -eq 1 ]]; then
+            legion_flags+=(
+                -DBUILD_MARCH= # to avoid -march=native for valgrind compatibility
+            )
+        fi
+        if [[ -n $FUZZER_LEGION_NETWORKS ]]; then
+            legion_flags+=(
+                -DLegion_NETWORKS=$FUZZER_LEGION_NETWORKS
+            )
+        fi
+        cmake "${legion_flags[@]}" ..
+        make install -j${FUZZER_THREADS:-4}
+        popd
     fi
-    if [[ -n $FUZZER_LEGION_NETWORKS ]]; then
-        legion_flags+=(
-            -DLegion_NETWORKS=$FUZZER_LEGION_NETWORKS
-        )
-    fi
-    cmake "${legion_flags[@]}" ..
-    make install -j${THREADS:-4}
     popd
 fi
-popd
 
 mkdir -p build
 pushd build
@@ -55,7 +62,7 @@ if [[ -n $FUZZER_LEGION_NETWORKS ]]; then
     )
 fi
 cmake "${fuzzer_flags[@]}" ..
-make -j${THREADS:-4}
+make -j${FUZZER_THREADS:-4}
 export REALM_SYNTHETIC_CORE_MAP=
-ctest --output-on-failure -j${THREADS:-4}
+./runner.py --fuzzer=$PWD/build/src/fuzzer -j${FUZZER_THREADS:-4}
 popd
