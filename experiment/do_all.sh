29e08a010d9c9ac2ef981b2351d19dcb7ccc6608
Elliott Slaughter <slaughter@cs.stanford.edu>
2024-07-26 11:03:12 -0700

    Better error checking in do_all.sh.

--- experiment/do_all.sh
+++ experiment/do_all.sh
@@ -4,9 +4,13 @@ machine="$1"
 
 set -e
 
+if [[ -z ${machine} ]]; then
+    echo "Please specify a machine to run this experiment for."
+    exit 1
+fi
+
 root_dir="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 cd "$root_dir"
-
 
 source experiment/$machine/env.sh
 ./experiment/$machine/do_build.sh
